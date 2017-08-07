72f472de5ce463e8b2193fe70333da63f9f85790
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2017-08-07 18:42:17 -0500

    Parameters Updated

--- oficial/control/config/parameters.php
+++ oficial/control/config/parameters.php
@@ -4,4 +4,4 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'joygle');
-define('APP_URL', 'http://localhost/Joygle/HouseSale-v2/oficial/control/');
+define('APP_URL', 'http://localhost:8000/control/');
